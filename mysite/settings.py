a30edcd056d26b3225a4cb88fbfd5a17ff041543
brownti159 <brownti159@gmail.com>
2021-05-14 16:42:52 -0400

    Altered the allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5bnej15l%vb4w&j!99)2^flh&qftj-btkt@%p&dlsjzcx+n5jl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere']
+ALLOWED_HOSTS = ['127.0.0.1', 'brownti159.pythonanywhere.com', ]
 
 
 # Application definition
